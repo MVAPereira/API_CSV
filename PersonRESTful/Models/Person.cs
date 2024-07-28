426d80015cf6ef372eb3580801bb45235e9e9a26
Marcos Pereira <102254761+MVAPereira@users.noreply.github.com>
2024-07-28 19:37:27 -0300

    add the object model and a csv test dataset

---
+++ PersonRESTful/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonRESTful.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  LastName { get; set; }
+        public string Zipcode { get; set; }
+        public string City { get; set; }
+        public string Color { get; set; }
+    }
+}
