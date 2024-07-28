a63d696d2cc70eacce74f5605112bdabe232170a
Marcos Pereira <102254761+MVAPereira@users.noreply.github.com>
2024-07-28 19:52:57 -0300

    create simple first controller for the Person object

---
+++ PersonRESTful/Controllers/PersonsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonRESTful.Models;
+using PersonRESTful.Services;
+
+namespace PersonRESTful.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly ICSVService _csvService;
+        public PersonsController(ICSVService csvService)
+        {
+            _csvService = csvService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeeCSV()
+        {
+            var Persons = _csvService.ReadCSV<Person>();
+
+            return Ok(Persons);
+        }
+    }
+}
