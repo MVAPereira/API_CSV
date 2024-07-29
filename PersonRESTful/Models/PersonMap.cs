9c65ff61253391ad25545f5067807ce6fa576c9d
Marcos Pereira <102254761+MVAPereira@users.noreply.github.com>
2024-07-29 14:29:39 -0300

    create method for Person map class that deals with zipcode and city

--- PersonRESTful/Models/PersonMap.cs
+++ PersonRESTful/Models/PersonMap.cs
@@ -1,4 +1,5 @@
-﻿using CsvHelper.Configuration;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace PersonRESTful.Models
 {
@@ -6,13 +7,35 @@ namespace PersonRESTful.Models
     {
         public PersonMap()
         {
-            Map(m => m.Id).Convert(args => args.Row.Context.Parser.Row); ;
+            Map(m => m.Id).Convert(args => GetId(args)); 
             Map(m => m.Name).Index(0);
             Map(m => m.LastName).Index(1);
-            Map(m => m.Zipcode).Index(2);
-            Map(m => m.City).Index(3);
-            Map(m => m.Color).Index(4);
+            Map(m => m.Zipcode).Convert(args => ExtractZipCode(args)); 
+            Map(m => m.City).Convert(args => ExtractCity(args));
+            Map(m => m.Color).Index(3);
         }
+
+
+        private int GetId(ConvertFromStringArgs args)
+        {
+            return Convert.ToInt32(args.Row.Context.Parser.RawRow);
+        }
+
+        private string ExtractZipCode(ConvertFromStringArgs args)
+        {
+            string value = args.Row.GetField<string>(2);
+            string valueCut = value.Substring(0, 5);
+            return valueCut;
+        }
+
+        private string ExtractCity(ConvertFromStringArgs args)
+        {
+            string value = args.Row.GetField<string>(2);
+            string city = value.Substring(5).Trim();
+            return city;
+        }
+
+
 
     }
 }
