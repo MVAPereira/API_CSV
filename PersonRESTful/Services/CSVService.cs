ada66773034992ba65b00f9b2ba8adafd1e54127
Marcos Pereira <102254761+MVAPereira@users.noreply.github.com>
2024-07-31 18:35:23 -0300

    add logic to the Person service for searching by color

--- PersonRESTful/Services/CSVService.cs
+++ PersonRESTful/Services/CSVService.cs
@@ -106,7 +106,63 @@ namespace PersonRESTful.Services
 
         public List<Person> GetPersonsByColor(string color)
         {
-            throw new NotImplementedException();
+            var reader = new StreamReader("Data/sample-input.csv");
+            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false
+            };
+
+            var csv = new CsvReader(reader, csvConfig);
+            csv.Context.RegisterClassMap<PersonMap>();
+
+            var records = new List<Person>();
+
+            while (csv.Read())
+            {
+                try
+                {
+                    var record = csv.GetRecord<Person>();
+
+                    if (string.IsNullOrEmpty(record.Name))
+                    {
+                        throw new ArgumentException("The field is empty!");
+                    }
+
+                    if (string.IsNullOrEmpty(record.LastName))
+                    {
+                        throw new ArgumentException("The field is empty!");
+                    }
+
+                    if (string.IsNullOrEmpty(record.Zipcode))
+                    {
+                        throw new ArgumentException("The field is empty!");
+                    }
+
+                    if (string.IsNullOrEmpty(record.City))
+                    {
+                        throw new ArgumentException("The field is empty!");
+                    }
+
+                    if (string.IsNullOrEmpty(record.Color))
+                    {
+                        throw new ArgumentException("The field is empty!");
+                    }
+
+                    if (record.Color == color)
+                    {
+                        records.Add(record);
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Validation error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return records;
         }
     }
 }
