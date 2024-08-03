6cfa634f5dce7babce46498bb0e5b951551c9d1f
Marcos Pereira <102254761+MVAPereira@users.noreply.github.com>
2024-08-03 18:43:22 -0300

    refactor Person service methods

--- PersonRESTful/Services/CSVService.cs
+++ PersonRESTful/Services/CSVService.cs
@@ -17,65 +17,8 @@ namespace PersonRESTful.Services
             _csvPath = csvPath;
             _classMap = classMap;
         }
-        public List<Person> GetAllPersons()
-        {
-            var reader = new StreamReader("Data/sample-input.csv");
-            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false
-            };
 
-            var csv = new CsvReader(reader, csvConfig);
-            csv.Context.RegisterClassMap<PersonMap>();
-
-            var records = new List<Person>();
-
-            while (csv.Read())
-            {
-                try
-                {
-                    var record = csv.GetRecord<Person>();
-
-                    if (string.IsNullOrEmpty(record.Name))
-                    {
-                        throw new ArgumentException("The field is empty!");
-                    }
-
-                    if (string.IsNullOrEmpty(record.LastName))
-                    {
-                        throw new ArgumentException("The field is empty!");
-                    }
-
-                    if (string.IsNullOrEmpty(record.Zipcode))
-                    {
-                        throw new ArgumentException("The field is empty!");
-                    }
-
-                    if (string.IsNullOrEmpty(record.City))
-                    {
-                        throw new ArgumentException("The field is empty!");
-                    }
-
-                    if (string.IsNullOrEmpty(record.Color))
-                    {
-                        throw new ArgumentException("The field is empty!");
-                    }
-                    
-                    records.Add(record);
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine($"Validation error: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            return records;
-        }
-
-        public Person GetPersonById(int personId)
+        private List<Person> ReturnValidPersons()
         {
             var reader = new StreamReader(_csvPath);
             var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
@@ -86,43 +29,7 @@ namespace PersonRESTful.Services
             var csv = new CsvReader(reader, csvConfig);
             csv.Context.RegisterClassMap(_classMap);
 
-            var person = new Person();
-            while (csv.Read())
-            {
-                try
-                {
-                    var record = csv.GetRecord<Person>();
-                    if (record.Id == personId)
-                    { 
-                        person = record;
-                        break;
-                    }
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine($"Validation error: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-            return person;
-        }
-
-        public List<Person> GetPersonsByColor(string color)
-        {
-            var reader = new StreamReader("Data/sample-input.csv");
-            var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false
-            };
-
-            var csv = new CsvReader(reader, csvConfig);
-            csv.Context.RegisterClassMap<PersonMap>();
-
-            var records = new List<Person>();
+            List<Person> persons = new List<Person>();
 
             while (csv.Read())
             {
@@ -155,21 +62,34 @@ namespace PersonRESTful.Services
                         throw new ArgumentException("The field is empty!");
                     }
 
-                    if (record.Color == color)
-                    {
-                        records.Add(record);
-                    }
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine($"Validation error: {ex.Message}");
+                    persons.Add(record);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
-            return records;
+            return persons;
+        }
+
+        public List<Person> GetAllPersons()
+        {
+            List<Person> persons = ReturnValidPersons();
+            return persons;
+        }
+
+        public Person GetPersonById(int personId)
+        {
+            List<Person> persons = ReturnValidPersons();
+            var person = persons.FirstOrDefault(p => p.Id == personId);
+            return person;
+        }
+
+        public List<Person> GetPersonsByColor(string color)
+        {
+            List<Person> persons = ReturnValidPersons().Where(p => p.Color == color).ToList();
+            return persons;
+
         }
     }
 }
