9ab107c5738daa4df2c0510dd1acb0674486c4c8
Marcos Pereira <102254761+MVAPereira@users.noreply.github.com>
2024-08-05 11:06:32 -0300

    add validation for the PersonJSON input object

--- PersonRESTful/Dto/PersonJSON.cs
+++ PersonRESTful/Dto/PersonJSON.cs
@@ -1,4 +1,6 @@
-﻿namespace PersonRESTful.Dto
+﻿using PersonRESTful.Models;
+
+namespace PersonRESTful.Dto
 {
     public class PersonJSON
     {
@@ -7,5 +9,28 @@
         public string Zipcode{ get; set; }
         public string City { get; set; }
         public string Color { get; set; }
+
+        public bool IsValidPersonJSON()
+        {
+
+            if 
+            (
+                string.IsNullOrWhiteSpace(LastName) ||
+                string.IsNullOrWhiteSpace(Name) ||
+                string.IsNullOrWhiteSpace(Zipcode) ||
+                string.IsNullOrWhiteSpace(City)
+            )
+            {
+                return false;
+            }
+
+            if (!Enum.IsDefined(typeof(Colors), Convert.ToInt32(Color)))
+            {
+                return false;
+            }
+
+            return true;
+
+        }
     }
 }
